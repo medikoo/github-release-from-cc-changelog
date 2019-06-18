852b0a39b329217925a3f0789184d6d3e0f746fa
Mariusz Nowak <mariusz@medikoo.com>
2019-06-18 13:17:56 +0200

    feat: github-release-from-cc-changelog CLI

---
+++ bin/github-release-from-cc-changelog.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+"use strict";
+
+require("essentials");
+
+const meta = require("../package");
+
+const argv = require("minimist")(process.argv.slice(2), {
+	boolean: ["help", "version"],
+	alias: { help: "h", version: "v" }
+});
+
+const [version] = argv._;
+
+const usage = `github-release-from-cc-changelog v${ meta.version }
+
+Usage: github-release-from-cc-changelog [-h | --help] <packageVersion>
+
+Resolve from CHANGELOG.md and puublish release notes of given version to GitHub
+(note: version tag should already be pushed to GitHub)
+
+Options:
+
+    --help,   -h  Show this message
+`;
+
+if (argv.help) {
+	process.stdout.write(usage);
+	return;
+}
+
+if (argv.version) {
+	process.stdout.write(`${ meta.version }\n`);
+	return;
+}
+
+if (!version) {
+	process.stdout.write(usage);
+	return;
+}
+
+require("../lib/private/cli")(version);
