d58d774e9603ad1be25356a2f00a0c050f672ca5
Mariusz Nowak <mariusz@medikoo.com>
2019-06-18 12:56:42 +0200

    feat: main resolveFromChangelog program

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const ensureString        = require("type/string/ensure")
+    , { resolve }         = require("path")
+    , UserError           = require("./lib/private/user-error")
+    , resolveRepoUrl      = require("./lib/private/resolve-repo-url")
+    , resolveReleaseNotes = require("./lib/resolve-release-notes")
+    , updateGithubRelease = require("./lib/update-github-release");
+
+module.exports = async (packagePath, version) => {
+	packagePath = resolve(ensureString(packagePath));
+	version = ensureString(version);
+
+	const repoUrl = resolveRepoUrl(packagePath);
+
+	const versionReleaseNotes = (await resolveReleaseNotes(
+		resolve(packagePath, "CHANGELOG.md")
+	)).get(version);
+
+	if (!versionReleaseNotes) {
+		throw new UserError(`Could not find release notes for version ${ version }`);
+	}
+
+	return updateGithubRelease(repoUrl, version, versionReleaseNotes);
+};
