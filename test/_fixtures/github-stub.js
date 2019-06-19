977c9c0a6aa01c89072ab02ae9bac6f15fe7cf7d
Mariusz Nowak <mariusz@medikoo.com>
2019-06-19 13:23:00 +0200

    refactor: Remove proxyquire specific props

--- test/_fixtures/github-stub.js
+++ test/_fixtures/github-stub.js
@@ -21,7 +21,7 @@ overrideEnv(() => {
 
 /* eslint-disable require-await */
 module.exports = require.cache[require.resolve("../../lib/private/github")].exports = {
-	"repos": {
+	repos: {
 		getReleaseByTag: async ({ tag }) => {
 			if (tag === "v0.0.1") throw ghResponses.getReleaseByTagName.error;
 			if (tag === "v0.1.3") throw ghResponses.getReleaseByTagName.error;
@@ -32,8 +32,6 @@ module.exports = require.cache[require.resolve("../../lib/private/github")].expo
 		createRelease: createReleaseSpy,
 		updateRelease: updateReleaseSpy
 	},
-	"@noCallThru": true,
-	"@global": true,
-	"_responses": ghResponses
+	_responses: ghResponses
 };
 /* eslint-enable */
